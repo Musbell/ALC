1b3a4c81c082ded24c4d74dc7b963934da568e60
Bello Musa <musbell007@gmail.com>
2017-10-09 05:05:56 +0100

    setup pwa

--- src/mongo-connector.js
+++
@@ -1,7 +0,0 @@
-const {MongoClient} = require('mongodb')
-
-const MONGO_URL = 'mongodb://musbell:password@ds013405.mlab.com:13405/alc'
-module.exports = async () => {
-  const db = await MongoClient.connect(MONGO_URL)
-  return {Students: db.collection('students')}
-}
--- src/schema/index.js
+++
@@ -1,9 +0,0 @@
-const {makeExecutableSchema} = require('graphql-tools')
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
-
-// Generate the schema object from type definition
-module.exports = makeExecutableSchema({
-  typeDefs,
-  resolvers
-})
